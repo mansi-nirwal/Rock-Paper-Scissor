5f57201ce8259e0ca7a269a1375ef7545eb1c818
Mansi Nirwal <manshinirwal19@gmail.com>
2025-06-09 20:10:16 +0530

    small project

---
+++ script.js
@@ -0,0 +1,17 @@
+
+   let computerMove='';
+function pickComputerMove(){
+    const randomNumber=Math.random();
+
+ 
+    if(randomNumber>= 0 && randomNumber < 1/3){
+        computerMove='rock'
+    }
+else if(randomNumber>=1/3 && randomNumber <2/3 )
+{
+    computerMove='paper'
+}
+else if (randomNumber >=2/3 && randomNumber < 1){
+computerMove='scissors';
+}
+}
